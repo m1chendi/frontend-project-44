5df3fc81d43f828c4bde2064780bf0f3715855dc
m1chendi <m1chendi.0@gmail.com>
2022-12-13 11:41:32 +0300

    cli linter

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,6 @@ const name = () => {
     console.log('Welcome to the Brain Games!');
     const userName = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${userName}`);
-}
+};
 
 export default name;
